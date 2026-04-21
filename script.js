a6a9ce4f83b1533a44f80dcf6bbb22e313965578
hermsfdf <145007303+hermsfdf@users.noreply.github.com>
2026-04-21 16:38:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,52 @@
 tg.ready();
-
 tg.expand();
 
-
-
 const user = tg.initDataUnsafe?.user;
-
 const userNameEl = document.getElementById("username");
-
 if (user && userNameEl) {
-
     userNameEl.innerText = user.first_name;
-
 }
-
-
 
 const greetings = ["Привет", "Рады видеть", "Ассалам алейкум", "Будь добрее сегодня"];
-
 const greetingEl = document.getElementById("greeting");
-
 if (greetingEl) {
-
     const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-
     greetingEl.textContent = randomGreeting;
-
 }
-
-
 
 function goToPage(pageId) {
-
     const pages = document.querySelectorAll('.page');
-
     pages.forEach(page => page.style.display = 'none');
 
-
-
     const activePage = document.getElementById(pageId);
-
     if (activePage) {
-
         activePage.style.display = 'block';
-
     }
-
     if (pageId === 'page1') {
-
             tg.BackButton.hide();
-
         } else {
-
             tg.BackButton.show();
-
         }
-
     }
-
-
 
 // Обработка кнопки "Назад" в интерфейсе Telegram
-
 tg.onEvent('backButtonClicked', () => {
-
     goToPage('page1');
-
 });
-
     
-
 const vibeBtn = document.getElementById("vibeBtn")
-
 if (vibeBtn) {
-
     vibeBtn.addEventListener("click", () => {
-
         tg.showAlert("В разработке");
-
         tg.HapticFeedback.notificationOccurred('success');
-
     });
-
 }
-
-
 
 const closeBtn = document.getElementById("closeBtn")
-
 if (closeBtn) {
-
     closeBtn.addEventListener("click", () => {
-
         tg.close()
-
     });
-
 }
-
-
 
 tg.setHeaderColor('secondary_bg_color');
